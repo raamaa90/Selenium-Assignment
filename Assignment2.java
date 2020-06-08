56bd376c2dfcb6c62880e1fb04d863e10cdc7205
Raamasubramanian A <raamaa90@gmail.com>
2020-06-08 19:38:56 +0530

    no message

---
+++ Assignment2.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("List of Odd between 1 to 20 is:");
+		for (int a=1; a<=20; a++) {
+			if(a%2 != 0) {
+				System.out.println(a);
+			}
+		}
+	}
+
+}
